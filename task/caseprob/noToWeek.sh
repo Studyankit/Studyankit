5f58de2f8b0ec62b342a424763b79595ee68bbee
Studyankit <ankitghosh2018@gmail.com>
2022-01-15 18:08:19 +0530

    no to check which day

---
+++ task/caseprob/noToWeek.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+read -p "Enter a no to check the day "  weekDay
+
+case $weekDay in
+	1)
+	echo "Monday"
+	;;
+	2)
+	echo "Tuesday"
+	;;
+	3)
+	echo "Wednesday"
+	;;
+	4)
+	echo "Thrusday"
+	;;
+	5)
+	echo "Friday"
+	;;
+	6)
+	echo "Saturday"
+	;;
+	7)
+	echo "Sunday"
+	;;
+	*)
+	echo "No not a day"
+esac
