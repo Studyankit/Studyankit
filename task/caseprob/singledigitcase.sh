dc44809d3420281d3ccb853f38d709c659b6801c
Studyankit <ankitghosh2018@gmail.com>
2022-01-15 17:59:49 +0530

    Single digit using case

---
+++ task/caseprob/singledigitcase.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+
+singleDigit=$((RANDOM%10))
+
+case $singleDigit in
+	1)
+	   echo "ONE"
+		;;
+	2)
+	   echo "TWO"
+		;;
+	3)
+	   echo "THREE"
+		;;
+	4)
+	   echo "FOUR"
+		;;
+	5)
+	   echo "FIVE"
+		;;
+	6)
+	   echo "SIX"
+		;;
+	7)
+	   echo "SEVEN"
+		;;
+	8)
+	   echo "EIGHT"
+		;;
+	9)
+	   echo "NINE"
+		;;
+	*)
+	   echo "No not a single digit"
+		;;
+esac
